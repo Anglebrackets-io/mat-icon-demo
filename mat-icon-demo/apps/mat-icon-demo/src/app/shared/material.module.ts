754a1023d2fd463a0f8b20a4d4b91492f2b7f9bf
Duncan Faulkner <duncan_faulkner@hotmail.com>
2019-10-07 22:32:10 +0100

    first example

---
+++ mat-icon-demo/apps/mat-icon-demo/src/app/shared/material.module.ts
@@ -0,0 +1,12 @@
+import { MatIconModule, MatIconRegistry } from '@angular/material/icon';
+import { NgModule } from '@angular/core';
+
+
+@NgModule({
+  declarations: [],
+  imports: [MatIconModule],
+  exports: [MatIconModule],
+  entryComponents: [],
+  providers: [MatIconRegistry]
+})
+export class MaterialModule { }
